d59de48fbdea8525ef098dc6313023bd84ba5425
AvinashDyasa <avidyasa60@gmail.com>
2022-04-01 21:18:42 +0530

    new

---
+++ lib/InputWrapper.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:proj/adminLogin/AdminLoginPage.dart';
+
+import 'Button.dart';
+import 'InputField.dart';
+import 'adminLogin/AdminLoginPage.dart';
+
+class InputWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(30),
+      child: Column(
+        children: <Widget>[
+          SizedBox(height: 40,),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10)
+            ),
+            child: InputField(),
+          ),
+          SizedBox(height: 40,),
+          Button(),
+          SizedBox(height: 40,),
+          IntrinsicHeight(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [TextButton(
+                child:
+                Text('New User'),
+                onPressed: (){},),
+                VerticalDivider(thickness: 1,color: Colors.black,),
+                TextButton(
+                  child: Text('Admin Login'),
+                  onPressed: ()=>AdminLoginPage,),],
+            ),
+          )
+          
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/adminLogin/AdminLoginPage.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import 'AdminHeader.dart';
+import 'AdminInputWrapper.dart';
+
+class AdminLoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(begin: Alignment.topCenter, colors: [
+            Colors.cyan.shade500,
+            Colors.cyan.shade300,
+            Colors.cyan.shade400,
+          ]),
+        ),
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 80,),
+            Header(),
+            Expanded(child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(60),
+                    topRight: Radius.circular(60),
+                  )
+              ),
+              child: InputWrapper(),
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
