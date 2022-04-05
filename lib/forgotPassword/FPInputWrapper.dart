231a94d74c97c28426f839f80ac9604144b80490
Prasad <81192162+Prasadhadbe@users.noreply.github.com>
2022-04-05 16:53:42 +0530

    .

--- lib/forgotPassword/FPInputWrapper.dart
+++ lib/forgotPassword/FPInputWrapper.dart
@@ -1,31 +1,31 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// import '../Button.dart';
-// import 'FPInputField.dart';
+import '../Button.dart';
+import 'FPInputField.dart';
 
-// class InputWrapper extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Padding(
-//       padding: EdgeInsets.all(30),
-//       child: Column(
-//         children: <Widget>[
-//           SizedBox(
-//             height: 40,
-//           ),
-//           Container(
-//             decoration: BoxDecoration(
-//                 color: Colors.white, borderRadius: BorderRadius.circular(10)),
-//             child: InputField(),
-//           ),
-//           SizedBox(
-//             height: 40,
-//           ),
-//           Button(
-//             isForgotPassword: true,
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
+class InputWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(30),
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 40,
+          ),
+          Container(
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(10)),
+            child: InputField(),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          Button(
+            isForgotPassword: true,
+          ),
+        ],
+      ),
+    );
+  }
+}
