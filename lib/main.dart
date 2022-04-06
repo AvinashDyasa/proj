fa9b5d76f4d15fe3284bd73753ee7fa779022003
AvinashDyasa <avidyasa60@gmail.com>
2022-04-06 14:53:11 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'LoginPage.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return new MaterialApp(
       debugShowCheckedModeBanner: false,
       home: LoginPage(),
