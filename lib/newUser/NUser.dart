dbee08ea13e2636ab72b69be459653f146092116
AvinashDyasa <avidyasa60@gmail.com>
2022-04-07 17:31:17 +0530

    Update NUser.dart

--- lib/newUser/NUser.dart
+++ lib/newUser/NUser.dart
@@ -12,197 +12,222 @@ class UserForm extends StatefulWidget {
 }
 
 class _UserFormState extends State<UserForm> {
-  TextEditingController _firstNameController = TextEditingController();
-  TextEditingController _lastNameController = TextEditingController();
-  TextEditingController _emailController = TextEditingController();
-  TextEditingController _passwordController = TextEditingController();
-  TextEditingController _societyNameContoller = TextEditingController();
-  TextEditingController _wingController = TextEditingController();
-  TextEditingController _flatNoController = TextEditingController();
+  final TextEditingController _firstNameController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController = TextEditingController();
+  final TextEditingController _societyNameContoller = TextEditingController();
+  final TextEditingController _wingController = TextEditingController();
+  final TextEditingController _flatNoController = TextEditingController();
 
-  bool _autoValidator = false;
-
-  static String validateEmptyField(
-      {required String text, required String fieldName}) {
-    return text.isEmpty ? "$fieldName cannot be empty" : "";
-  }
+  
 
   @override
   String dropdownvalue = "Owner";
   var items = ['Owner', 'Admin'];
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Container(
-        width: double.infinity,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(begin: Alignment.topCenter, colors: [
-            Colors.cyan.shade500,
-            Colors.cyan.shade300,
-            Colors.cyan.shade400,
-          ]),
+    final firstNameField = TextFormField(
+        autofocus: false,
+        controller: _firstNameController,
+        keyboardType: TextInputType.name,
+        validator: (value) {
+          RegExp regex = new RegExp(r'^.{3,}$');
+          if (value!.isEmpty) {
+            return ("First Name cannot be Empty");
+          }
+          if (!regex.hasMatch(value)) {
+            return ("Enter Valid name(Min. 3 Character)");
+          }
+          return null;
+        },
+        onSaved: (value) {
+          _firstNameController.text = value!;
+        },
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.account_circle),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "First Name",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
         ),
-        child: Column(
-          children: <Widget>[
-            SizedBox(
-              height: 80,
-            ),
-            Header(
-              isNewUser: true,
-            ),
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(60),
-                      topRight: Radius.circular(60),
-                    )),
-                child: Container(
-                  padding: const EdgeInsets.all(30),
-                  child: Form(
-                    autovalidateMode: _autoValidator
-                        ? AutovalidateMode.always
-                        : AutovalidateMode.disabled,
-                    child: SingleChildScrollView(
-                      child: Expanded(
-                        child: Column(
-                          children: [
-                            TextFormField(
-                              controller: _firstNameController,
-                              validator: (val) {
-                                return validateEmptyField(
-                                    fieldName: "First Name",
-                                    text: _firstNameController.text);
-                              },
-                              decoration: const InputDecoration(
-                                  labelText: 'First Name'),
-                            ),
-                            SizedBox(height: 10),
-                            TextFormField(
-                              controller: _lastNameController,
-                              validator: (val) {
-                                return validateEmptyField(
-                                    fieldName: "Last Name",
-                                    text: _firstNameController.text);
-                              },
-                              decoration:
-                                  const InputDecoration(labelText: 'Last Name'),
-                            ),
-                            SizedBox(height: 10),
-                            TextFormField(
-                              controller: _emailController,
-                              validator: (val) {
-                                if (!RegExp(
-                                        '^[a-zA-z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]')
-                                    .hasMatch(val!)) {
-                                  return ('Please enter a valid email');
-                                }
-                                return validateEmptyField(
-                                    fieldName: "Email",
-                                    text: _firstNameController.text);
-                              },
-                              decoration:
-                                  const InputDecoration(labelText: 'Email'),
-                            ),
-                            SizedBox(height: 10),
-                            TextFormField(
-                              obscureText: true,
-                              controller: _passwordController,
-                              validator: (val) {
-                                RegExp regex = new RegExp(r'^.{6,}$');
-                                if (!regex.hasMatch(val!)) {
-                                  return ("Please enter valid password(Min. 6 Char. long)");
-                                }
-                                return validateEmptyField(
-                                    fieldName: "Password",
-                                    text: _firstNameController.text);
-                              },
-                              decoration:
-                                  const InputDecoration(labelText: 'Password'),
-                            ),
-                            SizedBox(height: 10),
-                            TextFormField(
-                              controller: _societyNameContoller,
-                              validator: (val) {
-                                return validateEmptyField(
-                                    fieldName: "Society Name",
-                                    text: _firstNameController.text);
-                              },
-                              decoration: const InputDecoration(
-                                  labelText: 'Society Name'),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Container(
-                              width: double.infinity,
-                              child: DropdownButton(
-                                value: dropdownvalue,
-                                items: items.map((String items) {
-                                  return DropdownMenuItem(
-                                    value: items,
-                                    child: Text(items),
-                                  );
-                                }).toList(),
-                                onChanged: (String? newValue) {
-                                  setState(() {
-                                    dropdownvalue = newValue!;
-                                  });
-                                },
-                              ),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            TextFormField(
-                              controller: _wingController,
-                              validator: (val) {
-                                return validateEmptyField(
-                                    fieldName: "Wing",
-                                    text: _firstNameController.text);
-                              },
-                              decoration:
-                                  const InputDecoration(labelText: 'Wing'),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            TextFormField(
-                              controller: _flatNoController,
-                              validator: (val) {
-                                return validateEmptyField(
-                                    fieldName: "Flat no.",
-                                    text: _firstNameController.text);
-                              },
-                              decoration:
-                                  const InputDecoration(labelText: 'Flat no.'),
-                            ),
-                            ElevatedButton(
-                              onPressed: () {
-                                setState(() {
-                                  _autoValidator = true;
-                                });
-                              },
-                              child: Text(
-                                "Sign Up",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
+        );
+    final lastNameField = TextFormField(
+        autofocus: false,
+        controller: _lastNameController,
+        keyboardType: TextInputType.name,
+        validator: (value) {
+          if (value!.isEmpty) {
+            return ("Second Name cannot be Empty");
+          }
+          return null;
+        },
+        onSaved: (value) {
+          _lastNameController.text = value!;
+        },
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.account_circle),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Second Name",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),);
+    final emailField = TextFormField(
+        autofocus: false,
+        controller: _emailController,
+        keyboardType: TextInputType.emailAddress,
+        validator: (value) {
+          if (value!.isEmpty) {
+            return ("Please Enter Your Email");
+          }
+          // reg expression for email validation
+          if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
+              .hasMatch(value)) {
+            return ("Please Enter a valid email");
+          }
+          return null;
+        },
+        onSaved: (value) {
+          _emailController.text = value!;
+        },
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.mail),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Email",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ));
+    
+
+
+
+
+//     return Scaffold(
+//       resizeToAvoidBottomInset: false,
+//       body: Container(
+//         width: double.infinity,
+//         decoration: BoxDecoration(
+//           gradient: LinearGradient(begin: Alignment.topCenter, colors: [
+//             Colors.cyan.shade500,
+//             Colors.cyan.shade300,
+//             Colors.cyan.shade400,
+//           ]),
+//         ),
+//         child: Column(
+//           children: <Widget>[
+//             SizedBox(
+//               height: 80,
+//             ),
+//             Header(
+//               isNewUser: true,
+//             ),
+//             Expanded(
+//               child: Container(
+//                 decoration: BoxDecoration(
+//                     color: Colors.white,
+//                     borderRadius: BorderRadius.only(
+//                       topLeft: Radius.circular(60),
+//                       topRight: Radius.circular(60),
+//                     )),
+//                 child: Container(
+//                   padding: const EdgeInsets.all(30),
+//                   child: Form(
+                    
+//                     child: SingleChildScrollView(
+//                       child: Expanded(
+//                         child: Column(
+//                           children: [
+//                             TextFormField(
+//                               controller: _firstNameController,
+                              
+//                               decoration: const InputDecoration(
+//                                   labelText: 'First Name'),
+//                             ),
+//                             SizedBox(height: 10),
+//                             TextFormField(
+//                               controller: _lastNameController,
+                              
+//                               decoration:
+//                                   const InputDecoration(labelText: 'Last Name'),
+//                             ),
+//                             SizedBox(height: 10),
+//                             TextFormField(
+//                               controller: _emailController,
+                              
+//                               decoration:
+//                                   const InputDecoration(labelText: 'Email'),
+//                             ),
+//                             SizedBox(height: 10),
+//                             TextFormField(
+//                               obscureText: true,
+//                               controller: _passwordController,
+                              
+//                               decoration:
+//                                   const InputDecoration(labelText: 'Password'),
+//                             ),
+//                             SizedBox(height: 10),
+//                             TextFormField(
+//                               controller: _societyNameContoller,
+                              
+//                               decoration: const InputDecoration(
+//                                   labelText: 'Society Name'),
+//                             ),
+//                             SizedBox(
+//                               height: 10,
+//                             ),
+//                             Container(
+//                               width: double.infinity,
+//                               child: DropdownButton(
+//                                 value: dropdownvalue,
+//                                 items: items.map((String items) {
+//                                   return DropdownMenuItem(
+//                                     value: items,
+//                                     child: Text(items),
+//                                   );
+//                                 }).toList(),
+//                                 onChanged: (String? newValue) {
+//                                   setState(() {
+//                                     dropdownvalue = newValue!;
+//                                   });
+//                                 },
+//                               ),
+//                             ),
+//                             SizedBox(
+//                               height: 10,
+//                             ),
+//                             TextFormField(
+//                               controller: _wingController,
+                              
+//                               decoration:
+//                                   const InputDecoration(labelText: 'Wing'),
+//                             ),
+//                             SizedBox(
+//                               height: 10,
+//                             ),
+//                             TextFormField(
+//                               controller: _flatNoController,
+                              
+//                               decoration:
+//                                   const InputDecoration(labelText: 'Flat no.'),
+//                             ),
+                            
+//                           ],
+//                         ),
+//                       ),
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
